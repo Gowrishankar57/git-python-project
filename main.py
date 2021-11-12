baa46594cd5adac96ae0c9e00854b87e920a4720
sandeep <urstrulyshankar32@gmail.com>
2021-11-12 11:02:34 +0530

    files

---
+++ main.py
@@ -0,0 +1,9 @@
+#python code for palindrome number
+
+s=input()
+m=s
+r=s[::-1]
+if(r==m):
+      print("Palindrome")
+else:
+    print("Not Palindrome")
---
+++ main1.py
@@ -0,0 +1,7 @@
+# python code to check perfect sqaure or not
+n=int(input())
+s=n**(1/2)
+if (n==s*s):
+      print("Perfect sqaure")
+else:
+    print("Not a Perfect sqaure")
